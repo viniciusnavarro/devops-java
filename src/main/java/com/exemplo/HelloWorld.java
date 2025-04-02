c23038961cb887fe0f30ffd492b5a7cb01c5f943
Kerolen Rodrigues <kerolen_karina23@hotmail.com>
2025-04-01 21:28:35 -0300

    Ajuste mensagem

--- src/main/java/com/exemplo/HelloWorld.java
+++ src/main/java/com/exemplo/HelloWorld.java
@@ -2,6 +2,6 @@ package com.exemplo;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("\n\n Hello, World!");
+        System.out.println("\n\n Hello, DevOps!");
     }
 }
